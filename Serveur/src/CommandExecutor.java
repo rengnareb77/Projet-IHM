20eb579a4ca0e9e374f4db22eece4f129b474f5a
rengnareb77~ <luluozoir@gmail.com~>
2023-03-01 15:08:09 +0100

    Modification de l'indentation

--- Serveur/src/CommandExecutor.java
+++ Serveur/src/CommandExecutor.java
@@ -8,19 +8,24 @@ public class CommandExecutor {
 	public static void executeCommande(PrintStream ps, String commande) {
 		if(userOk && pwOk) {
 			// Changer de repertoire. Un (..) permet de revenir au repertoire superieur
-			if(commande.split(" ")[0].equals("cd")) (new CommandeCD(ps, commande)).execute();
+			if(commande.split(" ")[0].equals("cd"))
+				(new CommandeCD(ps, commande)).execute();
 	
 			// Telecharger un fichier
-			if(commande.split(" ")[0].equals("get")) (new CommandeGET(ps, commande)).execute();
+			if(commande.split(" ")[0].equals("get"))
+				(new CommandeGET(ps, commande)).execute();
 			
 			// Afficher la liste des fichiers et des dossiers du repertoire courant
-			if(commande.split(" ")[0].equals("ls")) (new CommandeLS(ps, commande)).execute();
+			if(commande.split(" ")[0].equals("ls"))
+				(new CommandeLS(ps, commande)).execute();
 		
 			// Afficher le repertoire courant
-			if(commande.split(" ")[0].equals("pwd")) (new CommandePWD(ps, commande)).execute();
+			if(commande.split(" ")[0].equals("pwd"))
+				(new CommandePWD(ps, commande)).execute();
 			
 			// Envoyer (uploader) un fichier
-			if(commande.split(" ")[0].equals("stor")) (new CommandeSTOR(ps, commande)).execute();
+			if(commande.split(" ")[0].equals("stor"))
+				(new CommandeSTOR(ps, commande)).execute();
 							}
 		else {
 			if(commande.split(" ")[0].equals("pass") || commande.split(" ")[0].equals("user")) {
