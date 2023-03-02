ee3ae9f5abe0ad31fe90e1c13e344b7cd1ce51ee
rengnareb77~ <luluozoir@gmail.com~>
2023-03-02 11:52:05 +0100

    Ajout de la commande LS

--- Serveur/src/CommandeLS.java
+++ Serveur/src/CommandeLS.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.PrintStream;
 
 public class CommandeLS extends Commande {
@@ -7,7 +8,41 @@ public class CommandeLS extends Commande {
 	}
 
 	public void execute() {
-		ps.println("La commande ls n'est pas encoré implémentée");
+		String path = (new File(".")).getAbsoluteFile().toString();
+		File directory = new File(path);
+		
+		if (!directory.exists()) {
+			ps.println("2 Le répertoire spécifié n'existe pas.");
+			return;
+		}
+		File[] files = directory.listFiles();
+		if (files == null) {
+			ps.println("2 Le répertoire spécifié n'est pas un répertoire.");
+			return;
+		}
+		if (files.length == 0) {
+			ps.println("0 Le répertoire spécifié est vide.");
+			return;
+		}
+		for (int i = 0; i < files.length ; i++) {
+			String number = "";
+			if (i == files.length - 1) {
+				number = "0 ";
+			} else {
+				number = "1 ";
+			}
+			
+			if (files[i].isDirectory()) {
+				ps.println(number + files[i].getName() + "/");
+			} else {
+				ps.println(number + files[i].getName());
+			}
+		}
+		
+		
+		
+		
+		
 	}
 
 }
