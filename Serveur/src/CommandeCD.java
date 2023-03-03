117c1aada54b73304feddf48aa6f50915f3775ef
rengnareb77~ <luluozoir@gmail.com~>
2023-03-03 08:18:22 +0100

    Ajout de la commande CD

--- Serveur/src/CommandeCD.java
+++ Serveur/src/CommandeCD.java
@@ -1,3 +1,4 @@
+import java.io.File;
 import java.io.PrintStream;
 
 public class CommandeCD extends Commande {
@@ -7,7 +8,30 @@ public class CommandeCD extends Commande {
 	}
 
 	public void execute() {
-		ps.println("La commande cd n'est pas encoré implémentée");
+		// Vérification du nombre d'arguments
+		if (commandeArgs.length < 1) {
+			ps.println("2 Il faut spécifier un répertoire");
+			return;
+		}
+		
+		String path = System.getProperty("user.dir");
+		File directory = new File(path + "/" + commandeArgs[0]);
+		// Vérification de l'existence du répertoire
+		if (!directory.exists()) {
+			ps.println("2 Le répertoire spécifié n'existe pas.");
+			return;
+		}
+		// Vérification que c'est un répertoire
+		if (!directory.isDirectory()) {
+			ps.println("2 Le répertoire spécifié n'est pas un répertoire.");
+			return;
+		}
+		
+		// Déplacement vers le répertoire (non fonctionnel)
+		System.setProperty("user.dir", directory.getAbsolutePath());
+		ps.println("0 Déplacement effectué vers " + commandeArgs[0]);
+		
+		System.out.println(directory.getAbsolutePath());
 	}
 
 }
